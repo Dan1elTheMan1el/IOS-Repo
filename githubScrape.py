ae97108161563ea6384454a7348f67f850e9f9b6
Daniel Friedman <friedmandaniel111@gmail.com>
2026-03-03 18:18:43 -0800

    MelonX + gitlab support

--- githubScrape.py
+++ githubScrape.py
@@ -2,6 +2,7 @@ import requests
 import json
 import markdown
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse, quote_plus
 
 myApps = json.load(open("resources/my-apps.json"))
 scraping = json.load(open("resources/scraping.json"))
@@ -15,39 +16,78 @@ readMe = readMe.replace("# MY APPS TABLE", myAppTable)
 scrapedAppTable = ""
 for repo_info in scraping:
     print(f"Scraping {repo_info['name']}...")
-    repo = repo_info["github"]
-    data = requests.get(f"https://api.github.com/repos/{repo}").json()
-
-    readme = requests.get(f"https://raw.githubusercontent.com/{repo}/refs/heads/main/README.md").text
-    html = markdown.markdown(readme)
-    soup = BeautifulSoup(html, 'html.parser')
 
     name = repo_info["name"]
-    author = data["owner"]["login"] if "owner" in data and "login" in data["owner"] else "Unknown"
-    subtitle = data["description"]
-    localizedDescription = soup.get_text()
     versions = []
 
-    print("Getting latest release...")
-    releases = requests.get(f"https://api.github.com/repos/{repo}/releases").json()
+    if "github" in repo_info:
+        repo = repo_info["github"]
+        data = requests.get(f"https://api.github.com/repos/{repo}").json()
 
-    for release in releases:
-        version = release["tag_name"].replace("v", "")
-        date = release["published_at"]
-        markdown_body = markdown.markdown(release["body"])
-        html_body = BeautifulSoup(markdown_body, 'html.parser')
-        for asset in release["assets"]:
-            if asset["browser_download_url"].endswith(".ipa"):
-                downloadURL = asset["browser_download_url"]
-                size = asset["size"]
-                break
-        versions.append({
-            "version": version,
-            "date": date,
-            "localizedDescription": html_body.get_text(),
-            "downloadURL": downloadURL,
-            "size": size
-        })
+        readme = requests.get(f"https://raw.githubusercontent.com/{repo}/refs/heads/main/README.md").text
+        html = markdown.markdown(readme)
+        soup = BeautifulSoup(html, 'html.parser')
+
+        author = data["owner"]["login"] if "owner" in data and "login" in data["owner"] else "Unknown"
+        subtitle = data["description"]
+        localizedDescription = soup.get_text()
+
+        print("Getting latest release...")
+        releases = requests.get(f"https://api.github.com/repos/{repo}/releases").json()
+
+        for release in releases:
+            version = release["tag_name"].lstrip("v")
+            date = release["published_at"]
+            markdown_body = markdown.markdown(release["body"])
+            html_body = BeautifulSoup(markdown_body, 'html.parser')
+            for asset in release["assets"]:
+                if asset["browser_download_url"].endswith(".ipa"):
+                    downloadURL = asset["browser_download_url"]
+                    size = asset["size"]
+                    break
+            versions.append({
+                "version": version,
+                "date": date,
+                "localizedDescription": html_body.get_text(),
+                "downloadURL": downloadURL,
+                "size": size
+            })
+    elif "gitlab" in repo_info:
+        host = urlparse(repo_info["gitlab"]).netloc
+        path = urlparse(repo_info["gitlab"]).path
+        data = requests.get(f"https://{host}/api/v4/projects/{quote_plus(path.lstrip('/'))}").json()
+
+        readme = requests.get(data['readme_url'].replace("/blob/", "/raw/")).text
+        html = markdown.markdown(readme)
+        soup = BeautifulSoup(html, 'html.parser')
+
+        subtitle = data["description"]
+        localizedDescription = soup.get_text()
+
+        print("Getting latest release...")
+        releases = requests.get(f"https://{host}/api/v4/projects/{quote_plus(path.lstrip('/'))}/releases").json()
+        author = releases[0]["author"]["name"] if "author" in releases[0] and "name" in releases[0]["author"] else "Unknown"
+
+        for release in releases:
+            version = release["tag_name"].lstrip("v")
+            date = release["released_at"]
+            markdown_body = markdown.markdown(release["description"])
+            html_body = BeautifulSoup(markdown_body, 'html.parser')
+            for asset in release["assets"]["links"]:
+                if asset["name"].endswith(".ipa"):
+                    downloadURL = asset["direct_asset_url"]
+                    size = None
+                    break
+            versions.append({
+                "version": version,
+                "date": date,
+                "localizedDescription": html_body.get_text(),
+                "downloadURL": downloadURL,
+                "size": size
+            })
+    else:
+        print(f"Unknown repo type for {repo_info['name']}")
+        continue
 
     bundleID = repo_info["bundleID"]
 
@@ -71,7 +111,8 @@ for repo_info in scraping:
     }
 
     myApps["apps"].append(app)
-    scrapedAppTable += f"|<img src=\"{iconURL}\" alt=\"{name}\" width=\"100\" height=\"100\" style=\"border-radius: 20px\">|[{name}](https://github.com/{repo})|{versions[0]['version']}|\n"
+    link = repo_info["gitlab"] if "gitlab" in repo_info else f"https://github.com/{repo_info['github']}"
+    scrapedAppTable += f"|<img src=\"{iconURL}\" alt=\"{name}\" width=\"100\" height=\"100\" style=\"border-radius: 20px\">|[{name}]({link})|{versions[0]['version']}|\n"
 readMe = readMe.replace("# AUTO SCRAPED TABLE", scrapedAppTable)
 
 print("Saving altstore-repo.json...")
